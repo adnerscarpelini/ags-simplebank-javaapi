95165f876b35c5ad7814e9413ee2fa10651f00ec
Adner Scarpelini <adner.scarpelini@gmail.com>
2025-07-14 13:46:08 -0300

    initialize project with Spring Boot using start.spring.io
    Generated project structure using start.spring.io with the following settings:
    - Language: Java
    - Build Tool: Maven
    - Java Version: 17
    
    Included dependencies:
    - Spring Web (RESTful applications with Spring MVC and Tomcat)
    - Spring Data JPA (JPA/Hibernate for SQL persistence)
    - H2 Database (in-memory SQL database with browser console)
    - Spring Security (authentication and access control)
    - Lombok (boilerplate code reduction with annotations)

---
+++ src/main/java/com/ags/ags_simplebank_javaapi/AgsSimplebankJavaapiApplication.java
@@ -0,0 +1,13 @@
+package com.ags.ags_simplebank_javaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgsSimplebankJavaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgsSimplebankJavaapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ags/ags_simplebank_javaapi/AgsSimplebankJavaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ags.ags_simplebank_javaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgsSimplebankJavaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
